713fc32b7ad918360a4d7726839ff4627d961863
Graham Jones <grahamjones139@gmail.com>
2014-01-01 14:58:36 +0000

    Added GPL licence information to source files.

--- kinect_version/benFinder/benFinder.py
+++ kinect_version/benFinder/benFinder.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python
 #
+#############################################################################
+#
+# Copyright Graham Jones, December 2013 
+#
+#  Original version by Joseph Howse in his book, "OpenCV Computer Vision with 
+#      Python" (Packt Publishing, 2013).
+#      http://nummist.com/opencv/
+#      http://www.packtpub.com/opencv-computer-vision-with-python/book
+#
+#############################################################################
+#
+#   This file is part of OpenSeizureDetector.
+#
+#    OpenSeizureDetector is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with OpenSeizureDetector.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+#
 # benFinder - my first go at using a depth camera to find benjamin in his 
 # bedroom - the first stage of looking for odd behaviour that could be
 # indicative of a seizure.
@@ -9,13 +36,7 @@
 # The background image is subtracted from each frame, which should make it
 # much easier to spot Benjamin if he is there!
 #
-#
-# Graham Jones, December 2013 (based on Joseph Howse's 'OpenCV Computer Vision 
-#    with Python' book - 
-#    http://www.packtpub.com/opencv-computer-vision-with-python/book)
-#
-#
-
+##############################################################################
 
 import cv2
 import numpy
--- kinect_version/benFinder/managers.py
+++ kinect_version/benFinder/managers.py
@@ -1,3 +1,32 @@
+#!/usr/bin/python
+#
+#############################################################################
+#
+# Copyright Graham Jones, December 2013 
+#
+#  Original version by Joseph Howse in his book, "OpenCV Computer Vision with 
+#      Python" (Packt Publishing, 2013).
+#      http://nummist.com/opencv/
+#      http://www.packtpub.com/opencv-computer-vision-with-python/book
+#
+#############################################################################
+#
+#   This file is part of OpenSeizureDetector.
+#
+#    OpenSeizureDetector is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with OpenSeizureDetector.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+#
 import cv2
 import numpy
 import time
