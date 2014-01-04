91b220ee8d6dbbcd74c0f65faa0d86eeae20b8ee
Graham Jones <grahamjones139@gmail.com>
2014-01-04 20:01:01 +0000

    Added simple peak finding algorithm - it works now!!!  calculates breathing rate, which falls to zero if you hold your breath!

--- kinect_version/benFinder/timeSeries.py
+++ kinect_version/benFinder/timeSeries.py
@@ -97,12 +97,26 @@ class TimeSeries:
             ts2 = self.smoothFlat(self._ts,30)
             times2 = self._times[:len(ts2)]  # Truncate times array to same lenght
             # Find all the peaks in the data.
-            peakind = signal.find_peaks_cwt(ts2, numpy.arange(30,60),min_snr=1.0)
+            #peakind = signal.find_peaks_cwt(ts2, numpy.arange(20,70),min_snr=1.0)
+            maxtab,mintab = peakdet(ts2,2)
+            print maxtab
+            if len(maxtab)>0:
+                peakind = maxtab[:,0].astype(int)
+            else:
+                peakind = []
+            #print peakind
+
+            ts_time = self._tslen / self._freq
+            rate = 60. * len(peakind)/ts_time # peaks per minute
+
+            print "%d peaks in %3.2f sec = %3.1f bpm" % \
+                (len(peakind),ts_time,rate)
 
             # Plot them
             pkx = []
             pky = []
             #print peakind,len(self._times), len(self._ts), len(ts2)
+            print "%d peaks found" % len(peakind)
             for peak in peakind:
                 pkx.append(times2[peak])
                 pky.append(ts2[peak])
@@ -131,3 +145,79 @@ class TimeSeries:
         w=numpy.ones(winLen,'d')
         v=numpy.convolve(w/w.sum(),ts,mode='valid')
         return v
+
+
+
+
+def peakdet(v, delta, x = None):
+    """
+    from https://gist.github.com/endolith/250860
+
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    
+    Returns two arrays
+    
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    %PEAKDET Detect peaks in a vector
+    %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    %        maxima and minima ("peaks") in the vector V.
+    %        MAXTAB and MINTAB consists of two columns. Column 1
+    %        contains indices in V, and column 2 the found values.
+    %      
+    %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    %        in MAXTAB and MINTAB are replaced with the corresponding
+    %        X-values.
+    %
+    %        A point is considered a maximum peak if it has the maximal
+    %        value, and was preceded (to the left) by a value lower by
+    %        DELTA.
+    
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    
+    """
+    maxtab = []
+    mintab = []
+       
+    if x is None:
+        x = numpy.arange(len(v))
+    
+    v = numpy.asarray(v)
+    
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+    
+    if not numpy.isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+    
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+    
+    mn, mx = numpy.Inf, -numpy.Inf
+    mnpos, mxpos = numpy.NaN, numpy.NaN
+    
+    lookformax = True
+    
+    for i in numpy.arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+        
+        if lookformax:
+            if this < mx-delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn+delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+ 
+    return numpy.array(maxtab), numpy.array(mintab)
