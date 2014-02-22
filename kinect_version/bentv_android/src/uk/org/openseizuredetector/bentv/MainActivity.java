de4111eb9e9d71e0a936034f6114b4c4a5cf05fb
Graham Jones <grahamjones139@gmail.com>
2014-02-22 22:23:14 +0000

    Got button clicking working eventually.

--- kinect_version/bentv_android/src/uk/org/openseizuredetector/bentv/MainActivity.java
+++ kinect_version/bentv_android/src/uk/org/openseizuredetector/bentv/MainActivity.java
@@ -5,14 +5,23 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 import android.widget.MediaController;
-import android.widget.VideoView;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.bentv_table_layout);
+
+	findViewById(R.id.pos1Button).setOnClickListener(mGlobal_OnClickListener);
+	findViewById(R.id.pos2Button).setOnClickListener(mGlobal_OnClickListener);
+	findViewById(R.id.pos3Button).setOnClickListener(mGlobal_OnClickListener);
+        findViewById(R.id.pos4Button).setOnClickListener(mGlobal_OnClickListener);
         
         String url = "rtsp://guest:guest@192.168.1.24/12";
                 
@@ -33,9 +42,39 @@ public class MainActivity extends Activity {
         //i.setData(Uri.parse(url));
         //startActivity(i);        
         
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.bentv_table_layout);
     }
+
+    //Global On click listener for all views
+    final OnClickListener mGlobal_OnClickListener = new OnClickListener() {
+        public void onClick(final View v) {
+            switch(v.getId()) {
+                case R.id.pos1Button:
+                    //Inform the user the button1 has been clicked
+                    Toast.makeText(getApplicationContext(), 
+				   "Button1 clicked.", 
+				   Toast.LENGTH_SHORT).show();                
+                break;
+                case R.id.pos2Button:
+                    //Inform the user the button2 has been clicked
+                    Toast.makeText(getApplicationContext(), 
+				   "Button2 clicked.", 
+				   Toast.LENGTH_SHORT).show();                
+                break;
+                case R.id.pos3Button:
+                    //Inform the user the button2 has been clicked
+                    Toast.makeText(getApplicationContext(), 
+				   "Button3 clicked.", 
+				   Toast.LENGTH_SHORT).show();                
+                break;
+                case R.id.pos4Button:
+                    //Inform the user the button2 has been clicked
+                    Toast.makeText(getApplicationContext(), 
+				   "Button4 clicked.", 
+				   Toast.LENGTH_SHORT).show();                
+                break;
+            }
+        }
+    };
 
 
     @Override
@@ -45,4 +84,8 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void onClick(View view) {
+
+    }
+
 }
