17d3e65ec8ec3b877f756d36fd5c595857702953
Graham Jones <grahamjones139@gmail.com>
2014-03-23 08:36:20 +0000

    jquerymobile viewer working fairly well now - still need to add alarm functions and sound from webcam.

--- kinect_version/benFinder/www/android2.js
+++ kinect_version/benFinder/www/android2.js
@@ -20,9 +20,9 @@ BF.modes = {0:"Multi Image",
 
 // Info on the images to be updated.  Each entry is:
 //   [Text Description, URL, ID of div to display image]
-BF.images = {0:["raw","/raw_img.png","#rawImg"],
-	     1:["masked","/masked_img.png","#maskedImg"],
-	     2:["chart","/masked_img.png","#chartImg"],
+BF.images = {0:["raw","/rawImg","#rawImg"],
+	     1:["masked","/maskedImg","#maskedImg"],
+	     2:["chart","/chartImg","#chartImg"],
 	     3:["webcam","http://guest:guest@192.168.1.24/tmpfs/auto.jpg","#webCamImg"]
 	     };
 
@@ -33,9 +33,24 @@ BF.timerID = -1;
 $(document).ready(function() {
     BF.mode = 0;  // Application mode.
     BF.imgNo = 0; // Image to display (in single image mode)
-    BF.setMode(0,0)
+    BF.setMode(0,0);  // Set to multi-image mode first.
+    BF.updateImages();
     //alert("BF.mode="+BF.mode+" = "+BF.modes[BF.mode]);
     BF.startTimer(BF.timerPeriod);
+    $(".rawImgLink").click(function() {BF.setMode(1,0);});
+    $(".maskedImgLink").click(function() {BF.setMode(1,1);});
+    $(".chartImgLink").click(function() {BF.setMode(1,2);});
+    $(".webCamLink").click(function() {BF.setMode(1,3);});
+    $(".homeLink").click(function() {BF.setMode(0,0);});
+
+    // There are lots of ways of getting back to the home page, so 
+    // capture them here, just in case they are missed.
+    $( document ).on( "pageshow", function( event, data ){
+	if (event.target.id=="home") {
+	    //alert("returning to multi-image mode")
+	    BF.setMode(0,0);
+	}
+    });
 });
 
 
@@ -52,30 +67,41 @@ BF.stopTimer = function() {
 }
 
 BF.setMode = function(mode,imgNo) {
-    //alert("BF.setMode("+mode+")");
+    // Change the mode of the application
+    // 0 = multi image view
+    // 1 = single image view
+    //alert("BF.setMode("+mode+","+imgNo+")");
     BF.mode = mode;
     BF.imgNo = imgNo;
+    if (BF.mode==0) {
+	$( ":mobile-pagecontainer" ).pagecontainer( "change", "#summaryData");
+    } else {
+	$( ":mobile-pagecontainer" ).pagecontainer( "change", "#singleImg");
+    }
 };
 
-BF.updateImages = function() {
 
+BF.updateImages = function() {
+    // Update the images by reloading them from the network.
+    // Which images to update depends on the application mode.
     if (BF.mode == 0) {
 	//alert("mode 0");
-	//BF.updateImage(0,BF.images[0][2]);
-	//BF.updateImage(1,BF.images[1][2]);
-	//BF.updateImage(2,BF.images[2][2]);
+	BF.updateImage(0,BF.images[0][2]);
+	BF.updateImage(1,BF.images[1][2]);
+	BF.updateImage(2,BF.images[2][2]);
 	BF.updateImage(3,BF.images[3][2]);
     }
     else {
 	//alert("mode 1 - BF.imgNo="+BF.imgNo);
-	BF.updateImage(BF.imgNo,"imgDiv");	
+	BF.updateImage(BF.imgNo,"#imgDiv");	
     }
 };
 
 BF.updateImage = function(imgNo,imgID) {
+    // Update an image - imageNo is the entry in the BF.images[] array
+    // imgId is the ID of the image element to update.
     url = BF.images[imgNo][1]+ "?" + (new Date()).getTime();;
-    imgID = BF.images[imgNo][2];
+    //imgID = BF.images[imgNo][2];
     //alert("url="+url+" id="+imgID);
     $(imgID).attr("src",url);
-
 };
