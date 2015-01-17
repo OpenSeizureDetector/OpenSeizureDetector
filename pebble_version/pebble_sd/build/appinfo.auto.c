1f3061f7c9f2b1868744ae34fc89988e3f3d50aa
Graham Jones <grahamjones139@gmail.com>
2015-01-17 21:14:51 +0000

    Progressing with pebble version - just displays accelerometer data on screen at the moment....

---
+++ pebble_version/pebble_sd/build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "pebble_sd",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x6D, 0xC7, 0x2D, 0xEF, 0x35, 0x9A, 0x48, 0x65, 0x89, 0xF9, 0x1F, 0x2D, 0x0B, 0x6C, 0x7F, 0x92 },
+  .virtual_size = 0xb6b6
+};
