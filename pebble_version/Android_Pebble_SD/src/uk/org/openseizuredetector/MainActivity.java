50e9c7376ebf71f4f9624d5c51b12b6e10c1576b
Graham Jones <grahamjones139@gmail.com>
2015-01-29 17:35:14 +0000

    Communications between pebble and android app now working - Android app will now display OK/WARNING/ALARM messages depending on fit detector output.

--- pebble_version/Android_Pebble_SD/src/uk/org/openseizuredetector/MainActivity.java
+++ pebble_version/Android_Pebble_SD/src/uk/org/openseizuredetector/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 import java.util.UUID;
 
 import com.getpebble.android.kit.Constants;
@@ -15,6 +17,10 @@ import com.getpebble.android.kit.util.PebbleDictionary;
 public class MainActivity extends Activity
 {
     private UUID SD_UUID = UUID.fromString("03930f26-377a-4a3d-aa3e-f3b19e421c9d");
+    private int KEY_ALARMSTATE = 2;
+    private int KEY_MAXVAL = 3;
+    private int KEY_MAXFREQ = 4;
+    private int KEY_SPECPOWER = 5;
 
     private PebbleKit.PebbleDataReceiver msgDataHandler = null;
     /** Called when the activity is first created. */
@@ -23,6 +29,21 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+	Button button = (Button)findViewById(R.id.button1);
+	button.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+		 TextView statusText = (TextView) findViewById(R.id.textView1);
+		 statusText.setText("button pressed!");
+		 PebbleDictionary data = new PebbleDictionary();
+		 data.addUint8(5, (byte) 42);
+		 data.addString(1, "A string"); 
+		 PebbleKit.sendDataToPebble(
+					    getApplicationContext(), 
+					    SD_UUID, 
+					    data);     
+             }
+	    });
+	onResume();
     }
 
     @Override
@@ -47,7 +68,7 @@ public class MainActivity extends Activity
 		    handler.post(new Runnable() {
 			    @Override
 			    public void run() {
-				updateUi();
+				updateUi(data);
 			    }
 			});
 		}
@@ -55,8 +76,32 @@ public class MainActivity extends Activity
 	PebbleKit.registerReceivedDataHandler(this,msgDataHandler);
     }
 
-    public void updateUi() {
-
+    public void updateUi(final PebbleDictionary data) {
+	String statusPhrase;
+	String alarmPhrase;
+	String viewText;
+	long alarmState = 0;
+	long maxVal = 0;
+	long maxFreq = 0;
+	long specPower = 0;
+	alarmState = data.getUnsignedIntegerAsLong(KEY_ALARMSTATE);
+	maxVal = data.getUnsignedIntegerAsLong(KEY_MAXVAL);
+	maxFreq = data.getUnsignedIntegerAsLong(KEY_MAXFREQ);
+	specPower = data.getUnsignedIntegerAsLong(KEY_SPECPOWER);
+	alarmPhrase = "Unknown";
+	if (alarmState==0) alarmPhrase="OK";
+	if (alarmState==1) alarmPhrase="WARNING";
+	if (alarmState==2) alarmPhrase="ALARM";
+	//alarmPhrase = data.toJsonString()+"alarmState="+alarmState;
+	//alarmPhrase = data.getString(10);
+	viewText = alarmPhrase + "\n " +
+	    "maxVal = "+maxVal+"\n" +
+	    "maxFreq = "+maxFreq+"\n" +
+	    "specPower = "+specPower +
+	    "\n" + data.toJsonString();
+	    
+	TextView statusText = (TextView) findViewById(R.id.textView1);
+	statusText.setText(viewText);
     }
 
     public void startWatchApp(View view) {
--- pebble_version/pebble_sd/src/pebble_sd.c
+++ pebble_version/pebble_sd/src/pebble_sd.c
@@ -49,6 +49,15 @@
 #define ALARM_SIZE 30  // pixels.
 #define SPEC_SIZE 30   // pixels
 
+
+/* Phone Communications */
+#define KEY_ALARMSTATE 2
+#define KEY_MAXVAL 3
+#define KEY_MAXFREQ 4
+#define KEY_SPECPOWER 5
+#define KEY_DATA 5
+
+
 /* GLOBAL VARIABLES */
 static Window *window;
 static TextLayer *text_layer;
@@ -71,6 +80,59 @@ int freqRes = 0;      // Actually 1000 x frequency resolution
 
 int alarmState = 0;    // 0 = OK, 1 = WARNING, 2 = ALARM
 int alarmCount = 0;    // number of seconds that we have been in an alarm state.
+
+
+/*************************************************************
+ * Communications with Phone
+ *************************************************************/
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Message received!");
+ // Get the first pair
+  Tuple *t = dict_read_first(iterator);
+
+  // Process all pairs present
+  while(t != NULL) {
+    // Process this pair's key
+    switch (t->key) {
+      case KEY_DATA:
+        APP_LOG(APP_LOG_LEVEL_INFO, "KEY_DATA received with value %d", (int)t->value->int32);
+        break;
+    }
+
+    // Get next pair, if any
+    t = dict_read_next(iterator);
+  }}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+/***************************************************
+ * Send some Seizure Detecto Data to the phone app.
+ */
+void sendSdData() {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  dict_write_uint8(iter,KEY_ALARMSTATE,(uint8_t)alarmState);
+  dict_write_uint32(iter,KEY_MAXVAL,(uint8_t)maxVal);
+  dict_write_uint32(iter,KEY_MAXFREQ,(uint8_t)maxFreq);
+  dict_write_uint32(iter,KEY_SPECPOWER,(uint8_t)specPower);
+  dict_write_cstring(iter,10,"a string");
+  app_message_outbox_send();
+
+}
+
+/*************************************************************
+ * Data Analysis
+ *************************************************************/
 
 /*********************************************
  * Returns the magnitude of a complex number
@@ -194,6 +256,11 @@ static void do_analysis() {
 }
 
 
+/***************************************************************************
+ * User Interface
+ ***************************************************************************/
+
+
 /************************************************************************
  * clock_tick_handler() - Analyse data and update display.
  * Updates the text layer clock_layer to show current time.
@@ -223,6 +290,9 @@ static void clock_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     //vibes_long_pulse();
     text_layer_set_text(alarm_layer, "** ALARM **");
   }
+
+  /* Send message to phone */
+  sendSdData();
 
   // Update data display.
   snprintf(s_buffer,sizeof(s_buffer),
@@ -381,6 +451,15 @@ static void init(void) {
 
   /* Subscribe to TickTimerService */
   tick_timer_service_subscribe(SECOND_UNIT, clock_tick_handler);
+
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), 
+		   app_message_outbox_size_maximum());
 }
 
 /**
