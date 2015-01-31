619b51bababa80c79465c23495496971c1427bb6
Graham Jones <grahamjones139@gmail.com>
2015-01-31 23:05:44 +0000

    Android App can now start web server, and returns alarm status on web page.

--- pebble_version/Android_Pebble_SD/src/uk/org/openseizuredetector/MainActivity.java
+++ pebble_version/Android_Pebble_SD/src/uk/org/openseizuredetector/MainActivity.java
@@ -13,50 +13,17 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.util.Log;
-import java.nio.ByteBuffer;
-import java.nio.ShortBuffer;
-import java.nio.ByteOrder;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.UUID;
 
-import com.getpebble.android.kit.Constants;
-import com.getpebble.android.kit.PebbleKit;
-import com.getpebble.android.kit.util.PebbleDictionary;
 
 
 public class MainActivity extends Activity
 {
     static final String TAG = "MainActivity";
-    private UUID SD_UUID = UUID.fromString("03930f26-377a-4a3d-aa3e-f3b19e421c9d");
-    private int NSAMP = 512;   // Number of samples in fft input dataset.
 
-    private int KEY_DATA_TYPE = 1;
-    private int KEY_ALARMSTATE = 2;
-    private int KEY_MAXVAL = 3;
-    private int KEY_MAXFREQ = 4;
-    private int KEY_SPECPOWER = 5;
-    private int KEY_SETTINGS = 6;
-    private int KEY_ALARM_FREQ_MIN =7;
-    private int KEY_ALARM_FREQ_MAX =8;
-    private int KEY_WARN_TIME = 9;
-    private int KEY_ALARM_TIME = 10;
-    private int KEY_ALARM_THRESH = 11;
-    private int KEY_POS_MIN = 12;       // position of first data point in array
-    private int KEY_POS_MAX = 13;       // position of last data point in array.
-    private int KEY_SPEC_DATA = 14;     // Spectrum data
-
-
-    // Values of the KEY_DATA_TYPE entry in a message
-    private int DATA_TYPE_RESULTS = 1;   // Analysis Results
-    private int DATA_TYPE_SETTINGS = 2;  // Settings
-    private int DATA_TYPE_SPEC = 3;      // FFT Spectrum (or part of a spectrum)
-
-
-    private short fftResults[];
     private Intent sdServerIntent;
 
-    private PebbleKit.PebbleDataReceiver msgDataHandler = null;
     final Handler serverStatusHandler = new Handler();
 
     /** Called when the activity is first created. */
@@ -64,25 +31,17 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-	// Allocate memory for the FFT spectrum results
-	fftResults = new short[NSAMP/2];
 
 	// Initialise the User Interface
         setContentView(R.layout.main);
 	Button button = (Button)findViewById(R.id.button1);
 	button.setOnClickListener(new View.OnClickListener() {
              public void onClick(View v) {
-		 TextView statusText = (TextView) findViewById(R.id.textView1);
-		 statusText.setText("button pressed!");
-		 PebbleDictionary data = new PebbleDictionary();
-		 data.addUint8(KEY_SETTINGS, (byte)1);
-		 data.addString(1, "A string"); 
-		 PebbleKit.sendDataToPebble(
-					    getApplicationContext(), 
-					    SD_UUID, 
-					    data);     
-             }
-	    });
+		 Log.v(TAG,"onClick(): "+v.toString());
+		 // request settings from pebble
+	     }
+	    }
+	    );
 
 	button = (Button)findViewById(R.id.button2);
 	button.setOnClickListener(new View.OnClickListener() {
@@ -108,19 +67,21 @@ public class MainActivity extends Activity
      * Based on http://stackoverflow.com/questions/7440473/android-how-to-check-if-the-intent-service-is-still-running-or-has-stopped-running
      */
     public boolean isServerRunning() {
-	Log.v(TAG,"isServerRunning()................");
+	//Log.v(TAG,"isServerRunning()................");
 	ActivityManager manager = 
 	    (ActivityManager) getSystemService(ACTIVITY_SERVICE);
 	for (RunningServiceInfo service : 
 		 manager.getRunningServices(Integer.MAX_VALUE)) {
-	    Log.v(TAG,"Service: "+service.service.getClassName());
+	    //Log.v(TAG,"Service: "+service.service.getClassName());
 	    if ("uk.org.openseizuredetector.SdServer"
 		.equals(service.service.getClassName())) {
+		//Log.v(TAG,"Yes!");
             return true;
-        }
+	    }
+	}
+	//Log.v(TAG,"No!");
+	return false;
     }
-    return false;
-}
 
 
     /*
@@ -150,101 +111,17 @@ public class MainActivity extends Activity
     @Override
     protected void onPause() {
 	super.onPause();
-	if (msgDataHandler != null) {
-	    unregisterReceiver(msgDataHandler);
-	    msgDataHandler = null;
-	}
     }
     @Override
     protected void onResume() {
 	super.onResume();
-	final Handler handler = new Handler();
-	msgDataHandler = new PebbleKit.PebbleDataReceiver(SD_UUID) {
-		@Override
-		public void receiveData(final Context context,
-					final int transactionId,
-					final PebbleDictionary data) {
-		    PebbleKit.sendAckToPebble(context,transactionId);
-		    // Do something with data
-		    // If we have a spectrum data packet, add it to the 
-		    // spectrum arry.
-		    if (data.getUnsignedIntegerAsLong(KEY_DATA_TYPE)
-			== DATA_TYPE_SPEC) {
-			int posMin = data.getUnsignedIntegerAsLong(KEY_POS_MIN).intValue();
-			int posMax = data.getUnsignedIntegerAsLong(KEY_POS_MAX).intValue();
-			// Read the data that has been sent, and convert it into
-			// an integer array.
-			byte[] byteArr = data.getBytes(KEY_SPEC_DATA);
-			ShortBuffer shortBuf = ByteBuffer.wrap(byteArr)
-			    .order(ByteOrder.BIG_ENDIAN)
-			    .asShortBuffer();
-			short[] shortArray = new short[shortBuf.remaining()];
-			shortBuf.get(shortArray);
-			for (int i=0;i<shortArray.length;i++) {
-			    fftResults[posMin+i] = shortArray[i];
-			}
-		    }
-		    handler.post(new Runnable() {
-			    @Override
-			    public void run() {
-				updateUi(data);
-			    }
-			});
-		}
-	    };
-	PebbleKit.registerReceivedDataHandler(this,msgDataHandler);
     }
 
-    public void updateUi(final PebbleDictionary data) {
+    public void updateUi() {
 	String statusPhrase;
 	String alarmPhrase;
 	String viewText = "Unknown";
-	long alarmState = 0;
-	long maxVal = 0;
-	long maxFreq = 0;
-	long specPower = 0;
-	if (data.getUnsignedIntegerAsLong(KEY_DATA_TYPE)==DATA_TYPE_RESULTS) {
-	    alarmState = data.getUnsignedIntegerAsLong(KEY_ALARMSTATE);
-	    maxVal = data.getUnsignedIntegerAsLong(KEY_MAXVAL);
-	    maxFreq = data.getUnsignedIntegerAsLong(KEY_MAXFREQ);
-	    specPower = data.getUnsignedIntegerAsLong(KEY_SPECPOWER);
-	    alarmPhrase = "Unknown";
-	    if (alarmState==0) alarmPhrase="OK";
-	    if (alarmState==1) alarmPhrase="WARNING";
-	    if (alarmState==2) alarmPhrase="ALARM";
-	    //alarmPhrase = data.toJsonString()+"alarmState="+alarmState;
-	    //alarmPhrase = data.getString(10);
-	    viewText = alarmPhrase + "\n " +
-		"maxVal = "+maxVal+"\n" +
-		"maxFreq = "+maxFreq+"\n" +
-		"specPower = "+specPower +
-		"\n" + data.toJsonString();
-	    TextView statusText = (TextView) findViewById(R.id.textView1);
-	    statusText.setText(viewText);
-	}
-	if (data.getUnsignedIntegerAsLong(KEY_DATA_TYPE)==DATA_TYPE_SETTINGS) {
-	    viewText = "Settings:\n"+data.toJsonString();
-	    TextView settingsText = (TextView) findViewById(R.id.textView2);
-	    settingsText.setText(viewText);
-	}
-	/*	if (data.getUnsignedIntegerAsLong(KEY_DATA_TYPE)==DATA_TYPE_SPEC) {
-	    
-	    String fftText = String.format("0=%06d, 10=%06d, 50=%06d",fftResults[0],fftResults[10],fftResults[50]);
-	    TextView settingsText = (TextView) findViewById(R.id.textView3);
-	    settingsText.setText(fftText);
-	}
-	*/
     }
 
-    public void startWatchApp(View view) {
-	PebbleKit.startAppOnPebble(getApplicationContext(),
-				   SD_UUID);
-
-    }
-
-    public void stopWatchApp(View view) {
-	PebbleKit.closeAppOnPebble(getApplicationContext(),
-				   SD_UUID);
-    }
 
 }
--- pebble_version/Android_Pebble_SD/src/uk/org/openseizuredetector/SdServer.java
+++ pebble_version/Android_Pebble_SD/src/uk/org/openseizuredetector/SdServer.java
@@ -6,6 +6,7 @@ import fi.iki.elonen.NanoHTTPD;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
+import android.content.Context;
 import android.content.Intent;
 import android.app.Service;
 import android.os.Bundle;
@@ -19,6 +20,15 @@ import android.os.Process;
 import android.util.Log;
 import java.io.*;
 import java.util.*;
+import java.util.UUID;
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
+import java.nio.ByteOrder;
+
+import com.getpebble.android.kit.Constants;
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
 
 /**
  * Based on example at:
@@ -28,11 +38,44 @@ import java.util.*;
  */
 public class SdServer extends Service
 {
+    private UUID SD_UUID = UUID.fromString("03930f26-377a-4a3d-aa3e-f3b19e421c9d");
+    private int NSAMP = 512;   // Number of samples in fft input dataset.
+
+    private int KEY_DATA_TYPE = 1;
+    private int KEY_ALARMSTATE = 2;
+    private int KEY_MAXVAL = 3;
+    private int KEY_MAXFREQ = 4;
+    private int KEY_SPECPOWER = 5;
+    private int KEY_SETTINGS = 6;
+    private int KEY_ALARM_FREQ_MIN =7;
+    private int KEY_ALARM_FREQ_MAX =8;
+    private int KEY_WARN_TIME = 9;
+    private int KEY_ALARM_TIME = 10;
+    private int KEY_ALARM_THRESH = 11;
+    private int KEY_POS_MIN = 12;       // position of first data point in array
+    private int KEY_POS_MAX = 13;       // position of last data point in array.
+    private int KEY_SPEC_DATA = 14;     // Spectrum data
+
+
+    // Values of the KEY_DATA_TYPE entry in a message
+    private int DATA_TYPE_RESULTS = 1;   // Analysis Results
+    private int DATA_TYPE_SETTINGS = 2;  // Settings
+    private int DATA_TYPE_SPEC = 3;      // FFT Spectrum (or part of a spectrum)
+
     private WebServer webServer;
     private final static String TAG = "SdServer";
     private Looper mServiceLooper;
     private ServiceHandler mServiceHandler;
     private SdData sdData;
+    private short fftResults[];
+    private PebbleKit.PebbleDataReceiver msgDataHandler = null;
+
+    private long alarmState;
+    private long maxVal;
+    private long maxFreq;
+    private long specPower;
+    private String alarmPhrase;
+    
 
     public SdServer() {
 	super();
@@ -59,6 +102,7 @@ public class SdServer extends Service
 	thread.start();
 	mServiceLooper = thread.getLooper();
 	mServiceHandler = new ServiceHandler(mServiceLooper);
+
     }
 
     /**
@@ -68,9 +112,12 @@ public class SdServer extends Service
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 	Log.v(TAG,"SdServer service starting");
+	// Allocate memory for the FFT spectrum results
+	fftResults = new short[NSAMP/2];
 	Message msg = mServiceHandler.obtainMessage();
 	msg.arg1 = startId;
 	mServiceHandler.sendMessage(msg);
+	startPebbleServer();
 	startWebServer();
 	return START_STICKY;
     }
@@ -83,7 +130,89 @@ public class SdServer extends Service
     @Override
     public void onDestroy() {
 	Log.v(TAG,"SdServer Service stopping");
+	if (msgDataHandler != null) {
+	    unregisterReceiver(msgDataHandler);
+	    msgDataHandler = null;
+	}
     }
+
+    private void startPebbleServer() {
+	Log.v(TAG,"StartPebbleServer()");
+	final Handler handler = new Handler();
+	msgDataHandler = new PebbleKit.PebbleDataReceiver(SD_UUID) {
+		@Override
+		public void receiveData(final Context context,
+					final int transactionId,
+					final PebbleDictionary data) {
+		    Log.v(TAG,"Received message from Pebble");
+		    PebbleKit.sendAckToPebble(context,transactionId);
+		    Log.v(TAG,"Acknowledged Message");
+		    Log.v(TAG,"Message is: "+data.toJsonString());
+		    if (data.getUnsignedIntegerAsLong(KEY_DATA_TYPE)
+			==DATA_TYPE_RESULTS) {
+			Log.v(TAG,"DATA_TYPE = Results");
+			Log.v(TAG,"Results Received from Pebble");
+			alarmState = data.getUnsignedIntegerAsLong(
+								   KEY_ALARMSTATE);
+			maxVal = data.getUnsignedIntegerAsLong(KEY_MAXVAL);
+			maxFreq = data.getUnsignedIntegerAsLong(KEY_MAXFREQ);
+			specPower = data.getUnsignedIntegerAsLong(KEY_SPECPOWER);
+			alarmPhrase = "Unknown";
+			if (alarmState==0) alarmPhrase="OK";
+			if (alarmState==1) alarmPhrase="WARNING";
+			if (alarmState==2) alarmPhrase="ALARM";
+		    }
+		    if (data.getUnsignedIntegerAsLong(KEY_DATA_TYPE)
+			==DATA_TYPE_SETTINGS) {
+			Log.v(TAG,"DATA_TYPE = Settings");
+			Log.v(TAG,"Settings:\n"+data.toJsonString());
+		    }
+
+		    if (data.getUnsignedIntegerAsLong(KEY_DATA_TYPE)
+			== DATA_TYPE_SPEC) {
+			Log.v(TAG,"DATA_TYPE = Spectrum");
+			/*			int posMin = data.getUnsignedIntegerAsLong(KEY_POS_MIN).intValue();
+			int posMax = data.getUnsignedIntegerAsLong(KEY_POS_MAX).intValue();
+			// Read the data that has been sent, and convert it into
+			// an integer array.
+			byte[] byteArr = data.getBytes(KEY_SPEC_DATA);
+			ShortBuffer shortBuf = ByteBuffer.wrap(byteArr)
+			    .order(ByteOrder.BIG_ENDIAN)
+			    .asShortBuffer();
+			short[] shortArray = new short[shortBuf.remaining()];
+			shortBuf.get(shortArray);
+			for (int i=0;i<shortArray.length;i++) {
+			    fftResults[posMin+i] = shortArray[i];
+			}
+			*/
+		    }
+		}
+	    };
+	PebbleKit.registerReceivedDataHandler(this,msgDataHandler);
+    }
+
+    public void getPebbleSdSettings() {
+	PebbleDictionary data = new PebbleDictionary();
+	data.addUint8(KEY_SETTINGS, (byte)1);
+	data.addString(1, "A string"); 
+	PebbleKit.sendDataToPebble(
+				   getApplicationContext(), 
+				   SD_UUID, 
+				   data);     
+    }
+
+    public void startWatchApp() {
+	PebbleKit.startAppOnPebble(getApplicationContext(),
+				   SD_UUID);
+
+    }
+
+    public void stopWatchApp() {
+	PebbleKit.closeAppOnPebble(getApplicationContext(),
+				   SD_UUID);
+    }
+
+
 
     protected void startWebServer() {
 	Log.v(TAG,"startWebServer()");
@@ -107,7 +236,9 @@ public class SdServer extends Service
                               Map<String, String> header,
                               Map<String, String> parameters,
                               Map<String, String> files) {
-            String answer = "SdServer Response";
+            String answer = "SdServer Response\n"
+		+alarmPhrase+"\n"
+		+"maxFreq = "+maxFreq;
             return new NanoHTTPD.Response(answer);
         }
     }
